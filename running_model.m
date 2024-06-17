aeddcf565446f88217938ad9c6e4fd548ff2ab41
Luiza Konkina <luiza.konkina@gmail.com>
2024-06-17 15:27:35 +0100

    Added code to interpolate LuxI over smallest time step of the simulation

--- running_model.m
+++ running_model.m
@@ -24,10 +24,44 @@ plot(time, AHLe)
 legend("Internal AHL", "External AHL")
 hold off
 %%
-% plotting
+% plotting time points
 indexoftime = 1:length(time);
 figure(3);
 hold on
 plot(indexoftime, time)
 legend("Time")
 hold off
+%% 
+% interpolation of the dataset
+listofdiff = zeros(2, 1);
+for i=1:length(time)
+    if i>1
+        difference = time(i)-time(i-1); % difference between current and previous time point
+        listofdiff = cat(2, listofdiff, [i;difference]); % add which time point it is and how big is the difference
+    end
+end
+listofdiff(2,1) = NaN; % ensure that first position is empty
+% what's the smallest time step?
+minn = min(listofdiff(2,:));
+
+% an array with evenly spaced time points, based on the smallest time step
+timeminn = 0:minn:500;
+
+% for now I will interpolate LuxI and imagine that it is same as GFP which
+% I would observe
+LuxIminn = interp1(time,LuxI, timeminn);
+% graph
+plot(time,LuxI,'o',timeminn,LuxIminn,':.');
+%% 
+% % alternative array with measurement every 10 mins, to replicate measuring
+% % system every 10 minutes
+% timetenm = 0:10:500;
+% % for now I will interpolate LuxI and imagine that it is same as GFP which
+% % I would observe
+% LuxItenm = interp1(time,LuxI, timetenm);
+% % graph
+% plot(time,LuxI,':.',timetenm,LuxItenm,'o'); % not a good choice, this
+% % misses peaks
+%% 
+
+
