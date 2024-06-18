826fd710ea7cc991100d195475a171534099edd3
Luiza Konkina <luiza.konkina@gmail.com>
2024-06-18 15:43:32 +0100

    Added annotations

--- compute_period.m
+++ compute_period.m
@@ -1,5 +1,5 @@
-function period = compute_period(s, dt)
+function period = compute_period(s, dt) % s is the signal, dt is the length of the time step
     ac = xcorr(s, s);
-    [~,locs]=findpeaks(ac);
-    period = mean(diff(locs(2:end-1))) * dt;
+    [~,locs]=findpeaks(ac); % find indexes of the peaks in ac
+    period = mean(diff(locs(2:end-1))) * dt; % calculate the index differences between peaks (with the exlusion  of 1st and last), find mean and multiply by the time step
 end
