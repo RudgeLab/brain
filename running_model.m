e725d47ea3d3c067ab572c342d85868f053bf514
Luiza Konkina <luiza.konkina@gmail.com>
2024-06-19 15:44:22 +0100

    Cleaned up comments

--- running_model.m
+++ running_model.m
@@ -1,5 +1,5 @@
 % running Danino model without coupling for 500 minutes
-danino(0, 0, 0, [0, 500], 1)
+danino(0, 0, 0, [0, 500], 0)
 %% 
 % assigning values
 AiiA = ans.y(1, :); %#ok<*NOANS>
@@ -15,6 +15,8 @@ time = ans.x;
 % plot(time, AiiA)
 % plot(time, LuxI)
 % legend("AiiA", "LuxI")
+% xlabel("Time(min)")
+% ylabel("AU")
 % hold off
 % 
 % figure(2);
@@ -22,24 +24,22 @@ time = ans.x;
 % plot(time, AHLi)
 % plot(time, AHLe)
 % legend("Internal AHL", "External AHL")
+% xlabel("Time(min)")
+% ylabel("AU")
 % hold off
 
 %% 
-% what's the smallest time step?
-tmin = min(diff(time));
+tmin = min(diff(time)); % smallest time step
+timeminn = 0:tmin:max(time); % an array with evenly spaced time points
+LuxIminn = interp1(time,LuxI, timeminn, "spline"); % interpolare LuxI
 
-% an array with evenly spaced time points, based on the smallest time step
-timeminn = 0:tmin:max(time);
-
-% for now I will interpolate LuxI and imagine that it is same as GFP which
-% I would observe
-LuxIminn = interp1(time,LuxI, timeminn, "spline");
-% % graph
+% graph
 % figure(4);
 % plot(time,LuxI,'o',timeminn,LuxIminn,':.');
+% legend("LuxI", "Intrapolated LuxI")
+% xlabel("Time(min)")
+% ylabel("AU")
 
-%% 
-% compute period based on LuxI
-LuxIperiod = compute_period(LuxIminn,tmin);
+LuxIperiod = compute_period(LuxIminn,tmin); % compute period based on LuxI
 
 
