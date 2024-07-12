71ce6441edc9f821d34e81906d1c6e613ab1910f
Luiza Konkina <luiza.konkina@gmail.com>
2024-07-12 12:17:44 +0100

    Added description of the function

--- prof_cos.m
+++ prof_cos.m
@@ -1,3 +1,3 @@
-function s = prof_cos(t, period, phase)
+function s = prof_cos(t, period, phase) % function to generate a cosine wave
     s = 0.5 * (1  + cos(2 * pi * t / period + phase));
 end
--- prof_pulse.m
+++ prof_pulse.m
@@ -1,3 +1,3 @@
-function s = prof_pulse(t, period, phase)
+function s = prof_pulse(t, period, phase) % function to generate a square wave
     s = 1 * (mod(t-phase*0.5*period/pi, period)/period < 0.5);
 end
