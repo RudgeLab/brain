ffc09dcb308cc9cda43338d93734ab112d8f7708
Luiza Konkina <luiza.konkina@gmail.com>
2024-08-01 11:29:51 +0100

    Added code to save figures on my PC

--- run_coupling.m
+++ run_coupling.m
@@ -14,7 +14,19 @@ LuxIeq = interp1(time,LuxI, timeq, "spline"); % interpolare LuxI
 LuxIperiod = compute_period(LuxIeq,t); % compute period based on LuxI 
 
 %% 
-% couplings = logspace(-9, 0, 10);
-couplings = linspace(0.0001, 0.001, 10);
+couplings = logspace(-9, 0, 10);
+% couplings = linspace(0.0001, 0.001, 10);
 SHIL_coupling(@danino, [0, 500], couplings, 2)
+%% 
 
+% SHIL(@danino, [0, 500], 0.0001, 2)
+%% 
+% code to save figures - PC specific
+FolderName = "C:\Users\Luiza\Desktop\Uni_code\Danino exp\SHIL_coupling";   % Your destination folder
+FigList = findobj(allchild(0), 'flat', 'Type', 'figure');
+for iFig = 1:length(FigList)
+  FigHandle = FigList(iFig);
+  FigName   = num2str(get(FigHandle, 'Number'));
+  set(0, 'CurrentFigure', FigHandle);
+  saveas(FigHandle,fullfile(FolderName, [FigName '.png'])); %Specify format for the figure
+end
